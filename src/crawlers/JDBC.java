7f2cc701b43dd7ab8f2ffdbd3fa41caee1da5ffd
YasserYka <yasseryka@gmail.com>
2019-09-28 13:35:36 +0300

    added maven and sql connector dependency and created JDBC class

---
+++ src/crawlers/JDBC.java
@@ -0,0 +1,11 @@
+package crawlers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JDBC {
+
+}

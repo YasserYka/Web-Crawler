38491813b6e67805d00e9cbd032480e72f7513ee
yasseryka <yasseryka@gmail.com>
2019-11-26 06:56:54 -0500

    added todo methods and created parser class for robotstxt

--- src/crawlers/Frontier.java
+++ src/crawlers/Frontier.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 public class Frontier {
 
 	private static HashSet<String> frontier;
-	private final int numberOfPagesToCrawl = 5; 
+	//Universal proposed waiting time before sending next request
+	private static final int WAITING_TIME = 60;
+	//used for testing proposed it should be removed/changed in production
+	private static final int NUMBER_OF_PAGES_TO_CRAWLER = 10;
 	
 	private Frontier() {}
 	
@@ -27,6 +30,9 @@ public class Frontier {
 	public void start(String seed) {
 		
 	}
+	private boolean isAbsolute() {
+		return false;
+	}
 	
 	//TODO: ask the DB if url have been visited?
 	private boolean isVisited(String url) {return false;}
@@ -34,5 +40,19 @@ public class Frontier {
 	//TODO: 
 	private void nextUrl() {
 		
+	}
+	private boolean isAllowed(String url) {
+		if(!RobotTXT.haveRobotTxt(url))
+			return false;
+		return true;
+	}
+	
+	public void makeRequest() {
+		
+	}
+	//To use Mercator's web crawler (if round trip is t, crawler should wait (t*10) second)
+	private int calculateRoundTrip() {
+		//Something like: int t = makeRequest();
+		return 0;
 	}
 }

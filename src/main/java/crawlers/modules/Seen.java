f49b5bcbb8e0325574543872f5e5482e694782d5
yasseryka <yasseryka@gmail.com>
2020-01-20 15:09:49 -0500

    added a bloomfilter to seen class and replaced jedis with redisson

--- src/main/java/crawlers/modules/Seen.java
+++ src/main/java/crawlers/modules/Seen.java
@@ -1,5 +1,24 @@
 package crawlers.modules;
 
+import org.redisson.Redisson;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+
 public class Seen {
 
+	RedissonClient redisson;
+	RBloomFilter<String> bloomFilter;
+	
+	public void init() {
+		//connect to it's default 127.0.0.1:6379
+		redisson = Redisson.create();
+		
+		//Retrieve bloom filter by it name
+		bloomFilter = redisson.getBloomFilter("bloomFilter");
+		
+		//The huge number on left is expected amount of insertions, on the right is expected false probability
+		bloomFilter.tryInit(55000000L, 0.03);
+	}
+	
+	public void shutdown() {redisson.shutdown();}
 }

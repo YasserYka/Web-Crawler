0eb4bb75c885a1eac89b199984c62437e1cd0195
yasseryka <yasseryka@gmail.com>
2020-02-03 07:17:17 -0500

    added base regex and more commenting and chaged name of method from drop to normilze in relativeUrlResolver

--- src/main/java/crawlers/modules/RelativeUrlResolver.java
+++ src/main/java/crawlers/modules/RelativeUrlResolver.java
@@ -1,28 +1,37 @@
 package crawlers.modules;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RelativeUrlResolver {
 	
-	private static final Pattern BASEIT_PATTERN = null;
-
+	/*
+	 * Stolen regex from stackoverflow url: https://stackoverflow.com/questions/24924072/website-url-validation-regex-in-java
+	 * gropu(0) should return base url if matcher
+	*/
+	private static final Pattern BASEIT_PATTERN = Pattern.compile("((http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+)).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?");
+	private static Matcher matcher;
 	/*
 	 * Three cases should be handled differently 
 	 * Relative URL start with '/', start with "./", start with "../" or start with non of them (that should be handled same as "./" but appends a slash to it)
 	 * "../" should be called recursively because it may contain more than one of those in relative URL
 	*/
 	public String resolve(String base, String relative) {
-		String resolved = null;
-		if(relative.startsWith("/"))
-			resolved = baseIt(base) + relative;
+		String resolved = null, based;
+		if(relative.startsWith("/")) {
+			based = baseIt(base);
+			if(based != null)
+				resolved = baseIt(base) + relative;
+		}
 		return resolved;
 	}
 	
 	//Takes full URL and delete all URI leaving the base URL
 	//Example http://www.notMaliciousAtAll.com/page/id it should return http://www.notmalicious.com
 	private String baseIt(String nonBase) {
-		return null;
+		matcher = BASEIT_PATTERN.matcher(nonBase);
+		return matcher.find() ? matcher.group(0) : null;
 	}
 	
 	//Takes full URL and returns it without file-name in path
@@ -54,11 +63,12 @@ public class RelativeUrlResolver {
 	}
 	
 	//Iterate through URLs and turn every relative to absolute URL
-	public static void drop(List<String> urls) {
+	public static void normalize(List<String> urls) {
 		int lengthOfUrls = urls.size(), i;
 		String url;
 		
 		for(i = 0; i < lengthOfUrls; i++) {
 			//Resolve if need to be resolved
+			//if resolve returns null drop URL
 		}
 	}}

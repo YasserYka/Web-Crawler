6305543b362ef71837eab1d18d639f89467e9f8d
YasserYka <yasseryka@gmail.com>
2021-04-03 17:28:22 +0300

    finished connection pool

--- src/main/java/crawlers/util/JDBCBean.java
+++ src/main/java/crawlers/util/JDBCBean.java
@@ -3,8 +3,11 @@ package crawlers.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
+
+import lombok.SneakyThrows;
 
 public class JDBCBean {
     
@@ -12,19 +15,35 @@ public class JDBCBean {
 	private static final String USERNAME = "user";
 	private static final String PASSWORD = "pass";
 
-	// to achieve connection pooling <Connection, IsUsed>
-	private static final HashMap<Connection, Boolean> CONNECTION_POOL;
+	// for connection pooling
+	private final List<Connection> connectionPool;
+	
     private static final int POOL_SIZE = 10;
 
-	// pre-warming database connections 
-    static {
-    
-		CONNECTION_POOL = new HashMap<Connection, Boolean>();
+	// pre-warming database connections
+	public JDBCBean() {
+		connectionPool = new ArrayList<Connection>();
 
-		IntStream.range(1, POOL_SIZE).forEach(i -> {
-			try { CONNECTION_POOL.put(DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD), false); }
-				catch (SQLException e) { e.printStackTrace(); }
-		});
+		IntStream.range(0, POOL_SIZE).forEach(i -> addConnection());
+	}
+ 
+	@SneakyThrows(SQLException.class)
+	public void addConnection(){
+
+		connectionPool.add(DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD));
+	}
+
+	public Connection getConnection(){
+
+		if (connectionPool.isEmpty())
+			throw new RuntimeException("Reached maximum database connection pool size!");
+
+		return connectionPool.remove(0);
+	}
+
+	public void realseConnection(Connection connection){
+
+		connectionPool.add(connection);
 	}
 
 }
